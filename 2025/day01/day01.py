3e401a575bbc3b697152b33f6d23cdde89ec6abb
leyew <102467346+itsme-zeix@users.noreply.github.com>
2025-12-01 15:51:32 +0800

    add 2025/day01

---
+++ 2025/day01/day01.py
@@ -0,0 +1,56 @@
+from typing import Optional
+
+def read_input(file_path) -> Optional[list[str]]:
+    try:
+        with open(file_path, 'r') as f:
+            return f.read().split()
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def part1(input: list[str]) -> int:
+    state = 50
+    res = 0
+
+    for op in input:
+        dir = op[0]
+        dist = int(op[1:])
+        if dir == 'L':
+            state = (state - dist) % 100
+        elif dir == 'R':
+            state = (state + dist) % 100
+        
+        if state == 0:
+            res += 1
+    
+    return res
+
+def part2(input: list[str]) -> int:
+    state = 50
+    res = 0
+
+    for op in input:
+        dir = op[0]
+        dist = int(op[1:])
+
+        full_rotations = dist // 100
+        dist %= 100
+        res += full_rotations
+
+        if dir == 'L':
+            if state > 0 and dist >= state:
+                res += 1
+            state = (state - dist) % 100
+        elif dir == 'R':
+            if state > 0 and dist >= (100 - state):
+                res += 1
+            state = (state + dist) % 100
+         
+    return res
+
+if __name__ == "__main__":
+    input = read_input("input.txt")
+    assert(input)
+    print(f"part 1: {part1(input)}")
+    print(f"part 2: {part2(input)}")
